39061ff582d332a8731e24fa5afdb82f0ae74746
Paul de Mast <pjf.demast@avans.nl>
2024-04-16 12:48:35 +0200

    Add project files.

---
+++ Swiper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Swiper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Swiper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Swiper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
